245dc0e44da5f3795e199eff829471fd2a01e976
Matthew Garry <mhgarry92@gmail.com>
2024-09-10 01:11:48 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+// import { OrbitControls } from "three/addons/loaders/GTLFLoader.js"
+// // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+
+// const controls = new OrbitControls(camera, renderer.domElement); // the camera is added to the scene as well as the renderer which is the canvas dom element
+// const loader = new GLTFLoader(); // create a loader to load gltf files
+
+
+const scene = new THREE.Scene(); // create a new Threejs scene
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // create a new camera
+/* the camera has 4 parameters:
+1. field of view (fov) in degrees
+2. aspect ratio
+3. near clipping plane
+4. far clipping plane
+*/
+const renderer = new THREE.WebGLRenderer();
+
+
+
+renderer.setSize(window.innerWidth, window.innerHeight); // set the size of the renderer to the window size
+document.body.appendChild(renderer.domElement); // append the renderer to the body of the documents
+const geometry = new THREE.BoxGeometry(1,1,1 ); // create a new box geometry
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // create a new material with a green color
+const cube = new THREE.Mesh(geometry, material); // create a new mesh with the geometry and material
+scene.add(cube); // add the cube to the scene
+
+camera.position.z = 5; // set the camera position
+
+function animate() {
+  renderer.render(scene, camera); // render the scene with the camera
+}
+
+renderer.setAnimationLoop(animate)
